dd1dd40e96026a91084a41298ec930ace9777477
mokemoko <mokemoko331@gmail.com>
2020-12-20 22:32:57 +0900

    conform gcs file path

--- functions/transform/csv.js
+++ functions/transform/csv.js
@@ -1,14 +1,13 @@
-const { readFileSync } = require('fs');
-const { resolve } = require('path');
 const { promisify } = require('util');
 const { get, set } = require('object-path');
 const parse = promisify(require('csv-parse'));
+const { read } = require('../util/file');
 
 // TODO: improve performance
 // use cache
 module.exports = async (conf, msg) => {
   const { file, match_column, match_path, res_column, res_path, nomatch_text } = conf;
-  const data = await parse(readFileSync(resolve(__dirname, file)));
+  const data = await parse(await read(file));
   const target = get(msg, match_path) || '';
   const matched = (data.find(r => target.match(new RegExp(r[match_column]))) || {})[res_column];
   set(msg, res_path, matched || nomatch_text);
