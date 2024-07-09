ae2b0ce052ec57ea14254f8b52aecb75c58ce099
mokemoko <mokemoko331@gmail.com>
2024-07-09 11:40:36 +0900

    improve: ignore post to slack when contents is empty.

--- functions/destination/slack.js
+++ functions/destination/slack.js
@@ -1,12 +1,16 @@
 const slack = require('../util/slack');
 
 function reflect(conf, msg) {
-  // TODO: 必要項目のみ上書くように
   delete conf.type;
   return Object.assign(msg, conf);
 }
 
 module.exports = async (conf, msg) => {
   const res = reflect(conf, msg);
+  if (!res.text && !res.attachments && !res.blocks) {
+    // 必須項目がない場合はAPIエラーを避けるためスキップ
+    console.log('[ignore] contents is empty.', JSON.stringify(res));
+    return;
+  }
   await slack.post(res);
 };
